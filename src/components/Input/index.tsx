2894948a81a39464a42ee06e7ac1ac5127a2941e
PauloFelippe <paulofelipperp.dev@gmail.com>
2020-05-21 12:55:43 -0300

    Form usability

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,9 @@
-import React, { useEffect, useRef } from 'react';
+import React, {
+  useEffect,
+  useRef,
+  useImperativeHandle,
+  forwardRef,
+} from 'react';
 import { TextInputProps } from 'react-native';
 import { useField } from '@unform/core';
 
@@ -13,11 +18,24 @@ interface InputValueReference {
   value: string;
 }
 
-const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => {
+interface InputRef {
+  focus(): void;
+}
+
+const Input: React.RefForwardingComponent<InputRef, InputProps> = (
+  { name, icon, ...rest },
+  ref,
+) => {
   const inputElementRef = useRef<any>(null);
 
   const { registerField, defaultValue = '', fieldName, error } = useField(name);
   const inputValueRef = useRef<InputValueReference>({ value: defaultValue });
+
+  useImperativeHandle(ref, () => ({
+    focus() {
+      inputElementRef.current.focus();
+    },
+  }));
 
   useEffect(() => {
     registerField<string>({
@@ -41,16 +59,16 @@ const Input: React.FC<InputProps> = ({ name, icon, ...rest }) => {
 
       <TextInput
         ref={inputElementRef}
+        keyboardAppearance="dark"
         placeholderTextColor="#666360"
         {...rest}
         defaultValue={defaultValue}
         onChangeText={value => {
           inputValueRef.current.value = value;
         }}
-        keyboardAppearance="dark"
       />
     </Container>
   );
 };
 
-export default Input;
+export default forwardRef(Input);
